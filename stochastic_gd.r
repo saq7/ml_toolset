a8b12966e9d47fa8f7955129f28e6064279f4807
saq7 <saq7@users.noreply.github.com>
2016-06-08 15:25:55 -0300

    Update stochastic_gd.r

--- stochastic_gd.r
+++ stochastic_gd.r
@@ -1,7 +1,10 @@
 
 ## Stochastic GRADIENT DESCENT ALGORITHM
-stochastic_gd<-function(design_matrix,response,gradient_fn,cost_fn,threshold=0.01,
-                        max_pass=1,init_weights=matrix(rep(0,ncol(design_matrix)),ncol=1),learning_rate=0.1){
+stochastic_gd<-function(design_matrix,response,
+                        gradient_fn,cost_fn,threshold=0.01,
+                        max_pass=1,
+                        init_weights=matrix(rep(0,ncol(design_matrix)),ncol=1),
+                        learning_rate=0.1){
   theta<-init_weights
   alpha<-learning_rate
   n<-nrow(design_matrix)
@@ -21,20 +24,3 @@ stochastic_gd<-function(design_matrix,response,gradient_fn,cost_fn,threshold=0.0
   }
   list(weights=theta,cost=cost)
 }
-
-## testing
-# set.seed(3122)
-# data_size<-1000
-# slope<-runif(1,min=0,max=1)
-# intercept<-runif(1,min=1,max=4)
-# X<-matrix(c(rep(1,data_size),runif(2*data_size,min=-5,max=5)),ncol=3,byrow=FALSE)
-# labels<- matrix(X[,2] > (slope*X[,1]+intercept),ncol=1)
-
-# system.time(stochastic_gd(design_matrix=X,response=labels,gradient_fn=ls_gradient_fn,
-#               cost_fn=ls_cost_fn,max_pass=200,learning_rate=0.001)->a)
-# a$weights->theta
-# 
-# pred<-X%*%theta
-# pred<-replace(pred,pred<0.5,0)
-# pred<-replace(pred,pred>0.5,1)
-# sum(labels!=pred)/nrow(labels)
